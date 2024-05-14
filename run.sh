7943ba3f9f56c8d86fe5439d6e65b58401953e4a
chiefexb <chief_exb@mail.ru>
2024-05-14 16:08:11 +0300

    123

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+docker network create my-network --driver bridge
+
 docker run --detach --rm --name openldap \
   --network my-network \
   --env LDAP_ADMIN_USERNAME=admin \
@@ -7,3 +9,6 @@ docker run --detach --rm --name openldap \
   --env LDAP_ROOT=dc=example,dc=org \
   --env LDAP_ADMIN_DN=cn=admin,dc=example,dc=org \
   bitnami/openldap:latest
+
+#  ldapsearch -x -b "cn=user1,ou=users,dc=example,dc=org" -H ldap://77.246.158.10:1389 -D "cn=admin,dc=example,dc=org" -W
+https://github.com/eea/eea.docker.sdi-proftpd/blob/master/proftpd/ldap.conf
